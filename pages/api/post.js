e19631c6e5033e030209565df63cd4b4bd6963f8
massedits <balathan2vijay004@gmail.com>
2023-12-11 19:35:05 +0530

    7:35pm 11dec

--- pages/api/post.js
+++ pages/api/post.js
@@ -1,11 +1,15 @@
 import { firestore } from "@/config";
 import { getDocs, collection } from "firebase/firestore";
 export default async function (req, res) {
-  const collectionData = collection(firestore, "post");
-  const data = await getDocs(collectionData, req);
-  const allDocs = data.docs.map((doc) => {
-    return doc.data();
-  });
+  try {
+    const collectionData = collection(firestore, "post");
+    const data = await getDocs(collectionData, req);
+    const allDocs = data.docs.map((doc) => {
+      return doc.data();
+    });
 
-  res.json({ message: allDocs });
+    res.json({ message: allDocs });
+  } catch (err) {
+    res.json({ message: "error" });
+  }
 }
--- pages/blog.js
+++ pages/blog.js
@@ -1,37 +1,34 @@
 import style from "/styles/blog.module.css";
 import Post from "@/component/post";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import GetRequest from "@/component/getRequest";
 
-export default function Blog({ jsonRes }) {
+export default function Blog() {
+  const [response, setResponse] = useState(null);
+
+  const resquest = async () => {
+    var data = await GetRequest("/post");
+    setResponse(data.message);
+  };
+
+  useEffect(() => {
+    resquest();
+  }, []);
+
   return (
     <div className="container">
       <div className={style.inner}>
         <div className={style.sideBar}></div>
         <div className={style.blog}>
           <h1>Blog</h1>
-          {jsonRes.map((value, index) => {
-            return <Post data={value} key={index} />;
-          })}
+          {response
+            ? response.map((value, index) => {
+                return <Post data={value} key={index} />;
+              })
+            : null}
         </div>
         <div className={style.sideBar}></div>
       </div>
     </div>
   );
-}
-
-export async function getStaticProps() {
-  const apiUrl =
-    process.env.NODE_ENV === "production"
-      ? `https://${process.env.VERCEL_URL}/api/post`
-      : "http://localhost:3000/api/post";
-
-  const response = await fetch(apiUrl);
-  const data = await response.json();
-  const jsonRes = data.message;
-
-  return {
-    props: {
-      jsonRes,
-    },
-  };
 }
