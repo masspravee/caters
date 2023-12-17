123ff4a7c162783a587dda6c6b106e58765b9f61
massedits <balathan2vijay004@gmail.com>
2023-12-17 07:22:52 +0530

    7:25 17dec

---
+++ pages/empty.js
@@ -0,0 +1,21 @@
+export default function Empty({ data }) {
+  return (
+    <div className="container">
+      <h1>{data}</h1>
+    </div>
+  );
+}
+
+export async function getStaticProps() {
+  const apiUrl =
+    process.env.NODE_ENV == "production"
+      ? "https://caters.vercel.app/api/empty"
+      : "http://localhost:3000/api/empty";
+  const response = await fetch(apiUrl);
+  const res = await response.json();
+  return {
+    props: {
+      data: res.message,
+    },
+  };
+}
