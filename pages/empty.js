f54829869c1a0904d3b5d58aeb43de355dc18275
massedits <balathan2vijay004@gmail.com>
2023-12-17 08:04:56 +0530

    7:41 17dec

--- pages/empty.js
+++ pages/empty.js
@@ -1,24 +1,21 @@
-import React, { Component, useEffect, useState } from "react";
-export default function Empty() {
-  const [dummy, setDummy] = useState(null);
-  const getter = async () => {
-    const apiUrl =
-      process.env.NODE_ENV == "production"
-        ? `${VERCEL_URL}/api/empty`
-        : "http://localhost:3000/api/empty";
-    console.log(apiUrl);
-    const response = await fetch(apiUrl);
-    const res = await response.json();
-    setDummy(res.message);
-  };
-
-  useEffect(() => {
-    getter();
-  }, []);
-
+export default function Empty({ data }) {
   return (
-    <div className="container">
-      <h1>{dummy}</h1>
+    <div>
+      <h1>{data}</h1>
     </div>
   );
+}
+
+export async function getStaticProps() {
+  const apiUrl =
+    process.env.NODE_ENV !== "production"
+      ? `https://caters.vercel.app/api/empty`
+      : "http://localhost:3000/api/empty";
+  const response = await fetch(apiUrl);
+  const res = await response.json();
+  return {
+    props: {
+      data: res.message,
+    },
+  };
 }
