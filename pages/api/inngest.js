85ba79767a59fb290d2618ace4076b52f94dfbdd
massedits <balathan2vijay004@gmail.com>
2024-05-01 16:26:33 +0530

    01/05 4:25

--- pages/api/inngest.js
+++ pages/api/inngest.js
@@ -3,7 +3,7 @@ import { inngestClient } from "@/worker/workLoad";
 import { tester } from "@/worker/post-handler";
 
 // Create a client to send and receive events
-console.log(process.env.INNGEST_SIGNING_KEY);
+console.log(process.env.INNGEST_EVENT_KEY);
 
 export default serve({
   client: inngestClient,
