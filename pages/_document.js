b7cb33d8e468e88a92b718b568c7e3e7525a8b31
massedits <balathan2vijay004@gmail.com>
2024-07-20 18:58:51 +0530

    adsense

--- pages/_document.js
+++ pages/_document.js
@@ -4,6 +4,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
+        <meta name="google-adsense-account" content="ca-pub-7274599009841717" />
         <link
           rel="stylesheet"
           href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,0,0"
