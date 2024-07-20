dbc47e32886659282d27e75841c68b249dc7a799
massedits <balathan2vijay004@gmail.com>
2024-07-20 19:06:27 +0530

    adsense

--- pages/_document.js
+++ pages/_document.js
@@ -9,6 +9,11 @@ export default function Document() {
           rel="stylesheet"
           href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,0,0"
         />
+        <script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7274599009841717"
+          crossorigin="anonymous"
+        ></script>
       </Head>
       <body>
         <Main />
